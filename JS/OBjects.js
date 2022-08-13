61cf6f784ed1f8dba8267caa696663e16ef7a88b
ahad4387 <ahad4387@gmail.com>
2022-08-13 21:09:35 +0530

    JS Objects

---
+++ JS/OBjects.js
@@ -0,0 +1,12 @@
+let a = new Object();
+new.name = 'Ahad';
+new.age = 24;
+new.hightestEducation = "Btech";
+new.subject ={
+    maths: 90,
+    English: 92;
+    Science: 95;
+}
+new.year = 2019;
+
+console.log(new);
