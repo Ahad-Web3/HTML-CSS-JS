ce7d2643539b91f2db4e6cff4e479577aaad66cf
Abdul Ahad <ahad4387@gmail.com>
2022-08-16 16:16:28 +0530

    Moved Files

---
+++ JS/Aug14/Aug14.js
@@ -0,0 +1,16 @@
+
+
+//adding two numbers
+let add = (n1,n2) => {
+ return n1 + n2;    
+};
+
+let sum = add(1,2);
+console.log(sum);
+
+//writing function in single line removing return and {}
+
+let sub = (m1,m2) => m1-m2
+
+//calling sub function
+sub(4,24);
