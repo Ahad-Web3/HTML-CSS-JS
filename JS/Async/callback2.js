077f62b556917c842a003a0f99984d8ac5cc3fba
Abdul Ahad <ahadweb3.0@gmail.com>
2022-09-11 15:26:16 +0530

    call back done

--- JS/Async/callback2.js
+++ JS/Async/callback2.js
@@ -8,13 +8,31 @@ let stock = {
 let order = (Fruit_name, call_production) => {
   setTimeout(() => {
     console.log(`Order placed and ${stock.Fruits[Fruit_name]} was selected`);
+    call_production(Fruit_name);
   },2000);
-  call_production(Fruit_name);
 };
 
 let production = (Fruit_name) => {
   setTimeout(() => {
-    console.log(`The ${stock.Fruits[Fruit_name]} has been cut`);
-  },2000);
+    console.log(`Production has started`);
+    setTimeout(()=>{
+      console.log(`The ${stock.Fruits[Fruit_name]} has been chopped`);
+      setTimeout(()=>{
+        console.log(`${stock.liquid[0]} and ${stock.liquid[1]} have been added`);
+        setTimeout(()=>{
+          console.log(`Machine has been started`);
+          setTimeout(()=>{
+            console.log(`Container:${stock.holder[0]} is selected`);
+            setTimeout(()=>{
+              console.log(`${stock.toppings[0]} has been selected as topping`)
+              setTimeout(()=>{
+                console.log(`Serve the Ice Cream`);
+              },2000);
+            },3000);
+          },2000);
+        },1000)
+      },1000)
+    },2000);
+  },0000);
 };
 order(1,production);
