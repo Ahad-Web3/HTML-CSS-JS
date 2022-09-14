113d5296df679ba4572d4ade6a037fccc4d184a7
Abdul Ahad <ahadweb3.0@gmail.com>
2022-09-14 18:21:55 +0530

    Organized files

---
+++ JS/Programs/prompt.js
@@ -0,0 +1,23 @@
+// const input = prompt();
+
+// console.log("Hello world!");
+// const input = prompt();
+
+var prompt = require('prompt');
+
+//
+// Start the prompt
+//
+prompt.start();
+
+//
+// Get two properties from the user: username and email
+//
+prompt.get(['username', 'email'], function (err, result) {
+  //
+  // Log the results.
+  //
+  console.log('Command-line input received:');
+  console.log('  username: ' + result.username);
+  console.log('  email: ' + result.email);
+});
--- JS/oddEven2.js
+++
@@ -1,3 +0,0 @@
-// var prompt = require('prompt');
-// prompt.start();
-prompt.get(['username', 'email']);
