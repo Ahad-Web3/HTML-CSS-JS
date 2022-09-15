a55c12a73c002290542dbeea11e08de5a6d915bc
Abdul Ahad <ahadweb3.0@gmail.com>
2022-09-15 07:31:32 +0530

    Check if a number is Positive, Negetive, or zero

---
+++ JS/Programiz/Check if a number is Positive, Negetive, or zero.js
@@ -0,0 +1,7 @@
+const prompt = require("prompt-sync")();
+let number = prompt("Enter a Number: ");
+
+if(number > 0){console.log(`${number} is positive number`);}
+else if(number < 0){console.log(`${number} is negetive number`);}
+else {console.log(`${number} is Zero number`);}
+
