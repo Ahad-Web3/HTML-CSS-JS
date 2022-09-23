3b12890b1e1c9557cd522449453905a466987aa7
Abdul Ahad <ahadweb3.0@gmail.com>
2022-09-23 16:21:17 +0530

    Changes to few files

---
+++ JS/Programs/galactic1.js
@@ -0,0 +1,69 @@
+/**
+* ! Fold(collapse) subregions: 
+* *        Ctrl + K + [
+* ! Unfold(uncollapse) subregions:
+* *        Ctrl + K + ]
+*/
+function region1 (){
+    function subRegion1 (){
+        // code goes here
+    }
+    function subRegion2 (){                                                    
+        // code goes here
+    }
+    function subRegion3 (){
+        // code goes here
+    }
+}
+
+
+
+
+
+function region2 (){
+    function subRegion1 (){
+        // code goes here
+    }
+    function subRegion2 (){
+        // code goes here
+    }
+    function subRegion3 (){
+        // code goes here
+    }
+}
+
+function region3 (){
+    function subRegion1 (){
+        // code goes here
+    }
+    function subRegion2 (){
+        // code goes here
+    }
+    function subRegion3 (){
+        // code goes here
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
